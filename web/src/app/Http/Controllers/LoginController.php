ebd3d2ac528ee4ecf63f059b7bc2944f5fb72d7f
Junel Solis <junel.solis@gmail.com>
2018-05-20 11:46:07 +0300

    login page

---
+++ web/src/app/Http/Controllers/LoginController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+
+class LoginController extends Controller
+{
+  public function main() {
+    return view('login');
+  }
+
+  public function login(Request $request) {
+    // validate request
+    $request->validate([
+      'username' => 'required|string',
+      'password' => 'required|string'
+    ]);
+
+    // assign variables
+    $username = $request['username'];
+    $password = $request['password'];
+
+    // encode password in sha1
+    $password = sha1($password);
+
+    // attempt to find admin user in DB
+    $admin = DB::table('admins')->where('username', $username)->first();
+
+    // return with error if user not found
+    if (empty($admin)) {
+      return back()->with('error', 'Invalid credentials.');
+    }
+
+    // check password against database
+    if ($password != $admin->password) {
+      return back()->with('error', 'Invalid credentials.');
+    }
+
+    // if password is correct, store some session variables
+    // then redirect to main admin view
+    session(['loggedIn' => true]);
+    session(['username' => $admin->username]);
+    session(['id' => $admin->id]);
+
+    return redirect('/admin');
+  }
+}
---
+++ web/src/resources/views/login.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0">
+
+  <!-- UIkit CSS -->
+  <link rel="stylesheet" href="{{ asset('css/uikit.min.css') }}" />
+  <!-- UIkit JS -->
+  <script src="{{ asset('js/uikit.min.js') }}"></script>
+  <script src="{{ asset('js/uikit.icons.min.js') }}"></script>
+
+  <link rel="stylesheet" href="{{ asset('css/login.css') }}" />
+  <title>FreeRADIUS Admin</title>
+</head>
+<body>
+  <div class="uk-container-small">
+    <div class="uk-position-center">
+      <div id="title">
+        <h2 class="uk-text-center"><strong><em>FreeRADIUS</em></strong>Admin</h2>
+      </div><br />
+      @if (session('error'))
+      <div class="uk-alert-warning" uk-alert>
+        <a class="uk-alert-close" uk-close></a>
+        <p>{{ session('error') }}</p>
+      </div>
+      @endif
+      <form method="post" action="/">
+        {{ csrf_field() }}
+        <fieldset class="uk-fieldset">
+          <div class="uk-margin">
+            <!-- <label>Username</label> -->
+            <input class="uk-input" type="text" placeholder="Username" name="username" required />
+          </div>
+          <div class="uk-margin">
+            <!-- <label>Password</label> -->
+            <input class='uk-input' type="password" placeholder="Password" name="password" required />
+          </div>
+          <div class="uk-margin">
+            <button class="uk-button uk-button-primary uk-width-1-1">Login</button>
+          </div>
+        </fieldset>
+      </form>
+      <div id="footer">
+        <br /><br />
+        <p>
+          This is open-source software created by Junel Solis.<br />Project repository on <a href="https://github.com/junelsolis/web-freeradius-mysql" target="_blank">Github</a>.
+        </p>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
