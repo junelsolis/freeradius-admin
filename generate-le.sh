70c7943713f25a7a7e48b884f1a513c01d168deb
Junel Solis <junel.solis@gmail.com>
2018-05-22 18:18:54 +0300

    Docker certbot script

--- generate-le.sh
+++ generate-le.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script will run a docker container
+# that will allow you to acquire a letsencrypt
+# certificate using the manual DNS method.
 docker run -it --rm \
   -v $(pwd)/le:/etc/letsencrypt \
   ekho/certbot \
