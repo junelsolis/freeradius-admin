117f97bac0a5f50fa332c6dbdb7f82c3a383e853
Junel Solis <junel.solis@gmail.com>
2018-08-14 12:05:16 +0300

    configure trustedproxies

--- web/src/config/app.php
+++ web/src/config/app.php
@@ -150,7 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Fideloper\Proxy\TrustedProxyServiceProvider::class,
         /*
          * Application Service Providers...
          */
