528d9663bd12282c03c0e144f50e60138a2070b7
Junel Solis <junel.solis@gmail.com>
2018-10-15 21:04:22 +0300

    add links to left sidebar

--- web/src/resources/views/left-sidebar.blade.php
+++ web/src/resources/views/left-sidebar.blade.php
@@ -2,18 +2,18 @@
   <div>
     <img src='/images/users-header.svg' width="100%" />
     <ul>
-      <li><a>Add User</a></li>
-      <li><a>Edit User</a></li>
-      <li><a>Directory</a></li>
+      <li><a href='/admin/users/add-user'>Add User</a></li>
+      <li><a href='/admin/users/modify-user'>Edit User</a></li>
+      <li><a href=''>Directory</a></li>
     </ul>
   </div>
 
   <div>
     <img src='/images/groups-header.svg' width='100%' />
     <ul>
-      <li><a>Add Group</a></li>
-      <li><a>Edit Group</a></li>
-      <li><a>Directory</a></li>
+      <li><a href='/admin/add-group'>Add Group</a></li>
+      <li><a href=''>Edit Group</a></li>
+      <li><a href='/admin/groups'>Directory</a></li>
     </ul>
   </div>
 
